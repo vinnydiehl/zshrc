2f1808e8b626527034e97120a5a9d70bdc89eafc
Vinny Diehl <vinny.diehl@gmail.com>
2023-07-08 02:23:59 -0400

    Add `git-cinnabar` to PATH

--- config/path.zsh
+++ config/path.zsh
@@ -5,6 +5,7 @@ addpath() {
 addpath $HOME/bin
 addpath $HOME/.local/bin
 addpath $HOME/.local/share/gem/ruby/3.0.0/bin
+addpath $HOME/.mozbuild/git-cinnabar
 addpath /usr/local/bin
 addpath /var/lib/snapd/snap/bin
 
